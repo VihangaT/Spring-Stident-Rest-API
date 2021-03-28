56f64f7e2beafa7f19265477a224d85f1d030b4b
VihangaT <liyanage.vihanga99@gmail.com>
2021-03-28 23:37:18 +0530

    Delete student implemented

--- src/main/java/com/vihanga/VihangaTestApi/student/StudentService.java
+++ src/main/java/com/vihanga/VihangaTestApi/student/StudentService.java
@@ -3,9 +3,11 @@ package com.vihanga.VihangaTestApi.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,29 @@ public class StudentService {
 
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+
+        boolean exist=studentRepository.existsById(studentId);
+        Student student= studentRepository.findById(studentId).orElseThrow(()-> new IllegalStateException(
+                "Student with id " +studentId+ " does not exist"
+        ));
+        if(name !=null && name.length() > 0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        } else{
+            throw new IllegalStateException("There is no change in the name");
+        }
+
+        if (email !=null && email.length() >0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional =studentRepository.findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("Email Already taken");
+            }else{
+                student.setEmail(email);
+            }
+
+        }
     }
 }
